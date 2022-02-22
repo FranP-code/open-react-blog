3e17264b5f8e865b35dac81182a85410ecaf8cde
Francisco Pessano <franpessano1@gmail.com>
2022-02-22 17:36:03 -0300

    Carpincho's page changed

--- src/components/Pages/PageOne.jsx
+++ src/components/Pages/PageOne.jsx
@@ -36,6 +36,28 @@ const PageOne = () => {
         window.location.href = './create-account'
     }
 
+    React.useEffect(() => {
+    
+        function consoleLog(seconds, message, image) {
+    
+          if (image) {
+    
+            console.image(image)
+            return
+          }
+    
+          setTimeout(() => {
+            console.log(message)
+          }, seconds * 1000);
+        }
+    
+        consoleLog(0, false, "https://c.tenor.com/0F1IPiIH2IAAAAAM/chimavara-capivara.gif")
+        consoleLog(3, "Oh! what a curious guy...")
+        consoleLog(4, "Don't forget to check the repo of this project:")
+        consoleLog(4.5, "https://github.com/FranP-code/open-react-blog")
+        consoleLog(7, "\nWTF console.image, why the npm module don't work and the images appear double?")  
+    }, [])
+
     return (
         <div className='page'>
             <MainHeader/>
