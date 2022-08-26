028d78bfa626adf2fcac9a74a7956a01071e6b75
Francisco Pessano <76450203+FranP-code@users.noreply.github.com>
2022-08-26 19:44:36 -0300

    Migrated from Heroku to Railway.app

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <footer>
             <p>Made with 💓 by <a href="https://franp.dev.ar" target="_blank" rel="noreferrer">Francisco Pessano</a></p>
-            <p><a href="https://open-react-blog-api.herokuapp.com" target="_blank" rel="noreferrer">API</a></p>
+            <p><a href="https://open-react-blog-api.up.railway.app" target="_blank" rel="noreferrer">API</a></p>
         </footer>
     )
 }
